727e295a00db4d6f399be23d11c7e5602fc26617
Darkcrafter07 <145045397+Darkcrafter07@users.noreply.github.com>
2026-07-07 12:34:30 +0300

    Fix console warning on UDMF Line Special #156
    Fix console warning on UDMF Line Special #156, the new height args are not considered garbage anymore.

--- src/actionspecials.h
+++ src/actionspecials.h
@@ -143,7 +143,7 @@ DEFINE_SPECIAL(Sector_SetHealth, 151, 3, 3, 3)
 
 DEFINE_SPECIAL(Teleport_NoStop, 154, 2, 3, 3)
 // portal specials
-DEFINE_SPECIAL(Line_SetPortal, 156, -1, -1, 4)
+DEFINE_SPECIAL(Line_SetPortal, 156, -1, -1, 5)
 // GZDoom/Vavoom specials 
 // Although ZDoom doesn't support them it's better to have them defined so that
 // WADs using them somewhere can at least be started without aborting due
